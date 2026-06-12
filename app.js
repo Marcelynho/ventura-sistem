53392ed179ef2902db0b7d38abeff68ef170c926
Marcelynho <109445266+Marcelynho@users.noreply.github.com>
2026-06-12 19:20:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 function entrar() {
-    const usuario = document.querySelector('input[type="text"]').value;
-    const senha = document.querySelector('input[type="password"]').value;
+  const usuario = document.querySelector('input[type="text"]').value;
+  const senha = document.querySelector('input[type="password"]').value;
 
-    if (usuario === "admin" && senha === "1234") {
-        alert("Login realizado com sucesso!");
-        window.location.href = "dashboard.html";
-    } else {
-        alert("Usuário ou senha incorretos.");
-    }
+  if (usuario === "admin" && senha === "1234") {
+    alert("Login realizado com sucesso!");
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Usuário ou senha incorretos.");
+  }
 }
 
 function salvarCliente() {
-    alert("Cliente salvo com sucesso!");
+  const nome = document.querySelectorAll("input")[0].value;
+  const telefone = document.querySelectorAll("input")[1].value;
+  const receita = document.querySelectorAll("input")[2].value;
+  const observacoes = document.querySelector("textarea").value;
+
+  const lista = document.getElementById("listaClientes");
+
+  lista.innerHTML += `
+    <p>
+      <strong>${nome}</strong><br>
+      Telefone: ${telefone}<br>
+      Receita: ${receita}<br>
+      Obs: ${observacoes}
+    </p>
+    <hr>
+  `;
+
+  alert("Cliente salvo com sucesso!");
 }
