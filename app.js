14bb6ef92903b8739782cfdc5539c02bb5051464
Marcelynho <109445266+Marcelynho@users.noreply.github.com>
2026-06-19 14:33:43 -0300

    Add button to send reminder based on status

--- app.js
+++ app.js
@@ -161,6 +161,12 @@ if (
         </button>
 
         <br><br>
+        ${status === "🟡 Vence hoje" ? `
+<button onclick="enviarLembrete('${cliente.telefone}', '${cliente.nome}')">
+📲 Enviar lembrete
+</button>
+<br><br>
+` : ""}
 
         <button onclick="editarCliente('${doc.id}')">
             Editar
