765744959af9cb649aacd7ac01787e2a7a13dbbd
Marcelynho <109445266+Marcelynho@users.noreply.github.com>
2026-07-07 12:04:06 -0300

    Fix condition to compare client name and client field

--- app.js
+++ app.js
@@ -1206,7 +1206,7 @@ const receita = doc.data();
 if (
 String(receita.cpf || "") === String(clienteEncontrado.cpf || "") ||
 String(receita.telefone || "") === String(clienteEncontrado.telefone || "") ||
-String(receita.nome || "").toLowerCase() === String(clienteEncontrado.nome || "").toLowerCase()
+String(receita.cliente || "").toLowerCase() === String(clienteEncontrado.nome || "").toLowerCase()
 ) {
 document.getElementById("odEsferico").value = receita.odLongeEsferico || "";
 document.getElementById("odCilindrico").value = receita.odLongeCilindrico || "";
